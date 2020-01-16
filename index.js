fa1663407d0cbabd91dbb2aa993460480035f696
mehedi <mdmehedirana@gmail.com>
2020-01-17 01:06:00 +0600

    ff

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => {
-    res.send({'hello':'there'});
-});
+require('./routes/dialogFlowRoutes').default(app);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
