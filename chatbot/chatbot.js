1450e1b51f78b44803c673905565ebb7d002de56
mehedi <mdmehedirana@gmail.com>
2020-01-26 00:31:16 +0600

    fgyh

---
+++ chatbot/chatbot.js
@@ -0,0 +1,50 @@
+'use strict'
+const dialogFlow = require('dialogflow').v2beta1;
+const config = require('../config/keys');
+const sessionClient = new dialogFlow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+
+
+module.exports = {
+
+    textQuery: async function (text, parameters = {}) {
+        let self = module.exports;
+        // The text query request.
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    // The query to send to the dialogflow agent
+                    text: text,
+                    // The language used by the client (en-US)
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            },
+
+            queryParams: {
+                payload: {
+                    data: parameters
+                }
+
+            }
+        };
+
+
+        // Send request and log result
+
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+
+    },
+
+
+    handleAction: function (responses) {
+        return responses;
+    }
+
+
+}
+//exports.chatbot = chatbot;
+//module.exports = chatbot;
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,10 +1,4 @@
-const dialogFlow = require('dialogflow').v2beta1;
-const config = require('../config/keys');
-
-const sessionClient = new dialogFlow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
-
-
+const chatbot = require('../chatbot/chatbot');
 module.exports = app => {
 
 
@@ -13,33 +7,13 @@ module.exports = app => {
   });
 
   app.post('/api/df_text_query', async (req, res) => {
+    let responses = await chatbot.textQuery(req.body.text,req.body.parameters);
 
-    // The text query request.
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          // The query to send to the dialogflow agent
-          text: req.body.text,
-          // The language used by the client (en-US)
-          languageCode: config.dialogFlowSessionLanguageCode,
-        },
-      },
-    };
-
-    // Send request and log result
-
-    let responses = await sessionClient
-    .detectIntent(request);
-
-
-
-
-    res.send(responses[0].queryResult)
+    res.send(responses[0].queryResult);
   });
 
   app.post('/api/df_event_query', (req, res) => {
-    res.send({ 'do': 'event query' });
+    res.send({ 'do': 'event query' })
   });
 
 }
