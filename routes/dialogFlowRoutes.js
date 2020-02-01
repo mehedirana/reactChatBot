ab50c84516fcd97bf7233a02092f395019310f42
mehedi <mdmehedirana@gmail.com>
2020-02-01 19:32:09 +0600

    connect and

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -15,7 +15,7 @@ module.exports = app => {
   app.post('/api/df_event_query', async (req, res) => {
     let responses = await chatbot.eventQuery(req.body.event,req.body.parameters);
 
-    res.send(responses[0].queryResult);
+    res.send(responses[1].queryResult);
   });
 
 }
