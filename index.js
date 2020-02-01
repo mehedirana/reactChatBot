3c842212e584546f065bed71239fd3380a93e31c
mehedi <mdmehedirana@gmail.com>
2020-02-02 02:52:48 +0600

    connect and

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const app = express();
-//const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -22,12 +22,12 @@ app.listen(PORT);
 
 
 
-/*const wakeUpDyno = (url, interval) => {
+const wakeUpDyno = (url, interval) => {
     const milliseconds = interval * 60000;
     setTimeout(() => {
         fetch(url);
     }, milliseconds);
-};*/
+};
 
-//module.exports = wakeUpDyno;
+module.exports = wakeUpDyno;
 //socket = io.listen(process.env.PORT);
