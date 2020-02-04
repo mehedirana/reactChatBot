65179b8286ff78ce20ee7336cf0f0abe9f535033
mehedi <mdmehedirana@gmail.com>
2020-02-05 01:42:49 +0600

    connect and server

--- index.js
+++ index.js
@@ -22,12 +22,32 @@ app.listen(PORT);
 
 
 
-const wakeUpDyno = (url, interval) => {
+/*const wakeUpDyno = (url, interval) => {
     const milliseconds = interval * 60000;
     setTimeout(() => {
         fetch(url);
     }, milliseconds);
-};
+};*/
 
-module.exports = wakeUpDyno;
+
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'));
+
+setInterval(console.log.bind(console, 'tick'), 1000);
+http.createServer((req, res) => res.end('hi'))
+  .listen(process.env.PORT || 3000, () => console.log('Listening'));
+
+function shutdown(signal) {
+  return (err) => {
+    console.log(`${ signal }...`);
+    if (err) console.error(err.stack || err);
+    setTimeout(() => {
+      console.log('...waited 5s, exiting.');
+      process.exit(err ? 1 : 0);
+    }, 5000).unref();
+  };
+}
+//module.exports = wakeUpDyno;
 //socket = io.listen(process.env.PORT);
