8ac46d464195df2ecc1ca9df2d97a98fdcaa408c
mehedi <mdmehedirana@gmail.com>
2020-02-17 23:26:03 +0600

    add dev

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ require('./routes/dialogFlowRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
+/*
+// Firing up the server on selected port
+SERVER.listen(PORT);
+
+SERVER.on("listening", () => {
+    console.log("[Server]::LISTEN:%s", PORT);
+});
+
+// Callback function for checking connecting or error
+SERVER.on("error", error => {
+    throw new Error(`[Server]::ERROR:${error.message}`);
+});
+*/
 
 
 
