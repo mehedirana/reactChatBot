2734f537097c3da203498f3bac3e2a77689d9d30
mehedi <mdmehedirana@gmail.com>
2020-02-23 15:01:28 +0600

    hhh

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import Header from './Header';
 import Landing from './pages/Landing';
 import About from './pages/About';
 import Shop from './shop/Shop';
+import Chatbot from './chatbot/Chatbot';
 
 const App = () => {
     return (
@@ -13,6 +14,9 @@ const App = () => {
             <div>
                 <Header/>
                 <Route exact path="/" component={Landing} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/shop" component={Shop}/>
+                <Chatbot/>
                 
             </div>
         </BrowserRouter>
