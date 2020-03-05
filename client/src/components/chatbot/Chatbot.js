6f2955ac582ad901597b8efd398374a104f5cdce
mehedi <mdmehedirana@gmail.com>
2020-03-05 16:43:40 +0600

    connect chatbot

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Chatbot = ()=>{
-    return(
-        <h2>Chatbot will be here</h2>
-    )
-};
+class Chatbot extends Component {
+    render() {
+        return (
+            <div style={{height:400, width:400, float:'right'}}>
+                <div id="chatbot" style={{height:'100%', width:'100%', overflow:'auto'}}>
+                    <h2>ChatBot</h2>
+                    <input type="text" />
+
+                </div>
+
+            </div>
+        )
+    }
+}
 
 export default Chatbot;
