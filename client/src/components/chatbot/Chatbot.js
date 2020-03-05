0eea3720a895c3a79582fbefa04b7efc08fe6c48
mehedi <mdmehedirana@gmail.com>
2020-03-05 17:48:59 +0600

    connect chatbot

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,6 +1,50 @@
 import React, { Component } from 'react';
-
+import axios from 'axios/index';
 class Chatbot extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            messages: []
+        }
+    }
+
+
+     async df_text_query(text){
+         let says = {
+             speaks: 'me',
+             msg: {
+                 text:{
+                     text: text
+                 }
+             }
+         };
+
+         this.setState({messages: [...this.state.messages,says]});
+        const res = await axios.post('/api/df_text_query',{text});
+
+        for(let msg of res.data.fullfillmentMessages){
+            says ={
+                speaks: 'bot',
+                msg: 'me'
+            }
+            this.setState({messages: [...this.state.messages, says]});
+        }
+    }
+
+    async df_evet_query(event){
+
+        const res = await axios.post('/api/df_event_query,{event}');
+
+        for(let msg of res.data.fullfillmentMessages){
+            let says ={
+                speaks : 'me',
+                msg: 'msg'
+            };
+            this.setState({messages: [...this.state.messages, says]});
+        }
+    }
+
     render() {
         return (
             <div style={{height:400, width:400, float:'right'}}>
