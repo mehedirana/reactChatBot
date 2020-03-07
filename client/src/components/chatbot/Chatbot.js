a431bfe05ca2446ab0cd494ef82bb4aa2b1d2611
mehedi <mdmehedirana@gmail.com>
2020-03-07 18:48:35 +0600

    gfb

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios/index';
+import Message from './Message';
 class Chatbot extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -10,46 +11,58 @@ class Chatbot extends Component {
     }
 
 
-     async df_text_query(text){
-         let says = {
-             speaks: 'me',
-             msg: {
-                 text:{
-                     text: text
-                 }
-             }
-         };
+    async df_text_query(text) {
+        let says = {
+            speaks: 'me',
+            msg: {
+                text: {
+                    text: text
+                }
+            }
+        };
 
-         this.setState({messages: [...this.state.messages,says]});
-        const res = await axios.post('/api/df_text_query',{text});
+        this.setState({ messages: [...this.state.messages, says] });
+        const res = await axios.post('/api/df_text_query', { text });
 
-        for(let msg of res.data.fullfillmentMessages){
-            says ={
+        for (let msg of res.data.fullfillmentMessages) {
+            says = {
                 speaks: 'bot',
                 msg: 'me'
             }
-            this.setState({messages: [...this.state.messages, says]});
+            this.setState({ messages: [...this.state.messages, says] });
         }
     }
 
-    async df_evet_query(event){
+    async df_evet_query(event) {
 
         const res = await axios.post('/api/df_event_query,{event}');
 
-        for(let msg of res.data.fullfillmentMessages){
-            let says ={
-                speaks : 'me',
+        for (let msg of res.data.fullfillmentMessages) {
+            let says = {
+                speaks: 'me',
                 msg: 'msg'
             };
-            this.setState({messages: [...this.state.messages, says]});
+            this.setState({ messages: [...this.state.messages, says] });
         }
+    }
+    renderMessages(stateMessages) {
+        if (stateMessages) {
+            return stateMessages.map((message, i) => {
+
+                return <Message key={i} speaks={message.speaks} text={message.msg.text.text} />;
+            });
+        } else {
+            return null;
+        }
+
     }
 
     render() {
         return (
-            <div style={{height:400, width:400, float:'right'}}>
-                <div id="chatbot" style={{height:'100%', width:'100%', overflow:'auto'}}>
+            <div style={{ height: 400, width: 400, float: 'right' }}>
+                <div id="chatbot" style={{ height: '100%', width: '100%', overflow: 'auto' }}>
                     <h2>ChatBot</h2>
+                    {this.renderMessages(this.state.messages)}
                     <input type="text" />
 
                 </div>
--- client/src/components/chatbot/Message.js
+++ client/src/components/chatbot/Message.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
-const Message = (props) =>(
+const Message = (props) => (
 
-    <div className ='col s12 m8 ofgfset-m2 offset-13'>
+    <div className="col s12 m8 ofgfset-m2 offset-13">
+        <div className="card blue-grey darken-1">
+            <div className="valign_wrapper">
+                {props.speaks === 'bot' &&
+                    <div className="col s2">
+                        <a className="btn-floating btn-large waves-effect waves-light red">{props.speaks}</a>
+
+                    </div>
+                }
+                <div className="col s12">
+                    <span className="black-text">
+                        {props.text}
+                    </span>
+
+                </div>
+                {props.speaks === 'me' &&
+                    <div className="col s2">
+                        <a className="btn-floating btn-large waves-effect waves-light red">{props.speaks}</a>
+
+                    </div>
+                }
+            </div>
+
+        </div>
 
     </div>
 );
+
+ export default Message;
