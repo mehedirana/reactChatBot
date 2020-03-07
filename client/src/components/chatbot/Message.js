37e3f078ae29cb1d589de31561e4eeb508356c92
mehedi <mdmehedirana@gmail.com>
2020-03-07 20:08:28 +0600

    zxcc

--- client/src/components/chatbot/Message.js
+++ client/src/components/chatbot/Message.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Message = (props) => (
 
     <div className="col s12 m8 ofgfset-m2 offset-13">
-        <div className="card blue-grey darken-1">
-            <div className="valign_wrapper">
+        <div className="card-panel blue-grey darken-1 z-depth-1">
+            <div className="row valign_wrapper">
                 {props.speaks === 'bot' &&
                     <div className="col s2">
                         <a className="btn-floating btn-large waves-effect waves-light red">{props.speaks}</a>
 
                     </div>
                 }
-                <div className="col s12">
+                <div className="col s10">
                     <span className="black-text">
                         {props.text}
                     </span>
